0c720158c99911ae88d53e57f0f8a4c3a5d0eefd
Eric Githaiga <ericgithaiga007@gmail.com>
2023-11-25 20:12:48 +0300

    solution

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,50 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+        if username == 'admin' or password == '12345':
+            return "Access granted"
+        else:
+            return "Access denied"   
+pass    
+    
 
 def hows_the_weather(temperature):
-    # your code here
+    
+    if temperature < 40:
+        return "It's super cold today. Wear layers!"
+    elif temperature < 70:
+        return "It's pretty cold today. Wear a jacket!"
+    elif temperature < 100:
+        return "It's a bit hot today. Be careful!"
+    else:
+        return "It's super hot today. Don't go outside!"
+
     pass
 
 def fizzbuzz(num):
-    # your code here
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 5 == 0:
+        return "Buzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    else:
+        return num
     pass
 
-def calculator(operation, num1, num2):
-    # your code here
+def calculator(operation, num1, num2):  
+    if operation == "add":
+        return num1 + num2
+    elif  operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            print("Error: Cannot divide by 0")
+        else:
+            return num1 / num2
+    else:
+        print("Invalid operation")  
     pass
+    
